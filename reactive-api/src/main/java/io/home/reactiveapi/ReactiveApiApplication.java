8bb0841b764800abd37a5df15ffa9b2f01edad37
Vipul Kumar Sharma <tysonvks@gmail.com>
2020-02-16 14:11:55 +0530

    Initial Commit

---
+++ reactive-api/src/main/java/io/home/reactiveapi/ReactiveApiApplication.java
@@ -0,0 +1,13 @@
+package io.home.reactiveapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveApiApplication.class, args);
+	}
+
+}
